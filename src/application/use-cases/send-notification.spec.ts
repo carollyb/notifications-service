62641eca5b9bf61cbf07997cf4f7c8068bf7722f
carollyb <carolly.barbosa@gmail.com>
2022-12-17 17:10:04 -0300

    chore: creates repositories

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,15 +1,18 @@
 import { randomUUID } from 'crypto';
 import { SendNotification } from './send-notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
     const { notification } = await sendNotification.execute({
       category: 'social',
       content: 'This is a notification',
       recipientId: randomUUID(),
     });
 
-    expect(notification).toBeTruthy();
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
